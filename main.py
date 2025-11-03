d09b083c5b365c8af51d5e3523c4f30dd156af2b
Matti <m0527176210@gmail.com>
2025-11-03 19:58:12 +0200

    the updats project

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ from core.deck import build_standard_deck, shuffle_by_suit
 from core.game_logic import run_full_game, deal_two_each, dealer_play
 from core.player_io import ask_player_action
 
-player = {"hand":[]}
-dealer = {"hand":[]}
+def init_game():
+    player = {"hand":[]}
+    dealer = {"hand":[]}
 
-deck = build_standard_deck()
-shuffle_by_suit(deck)
-deal_two_each(deck, player, dealer)
-dealer_play(deck, dealer)
-ask_player_action()
-
+    deck = build_standard_deck()
+    shuffle_by_suit(deck)
+    deal_two_each(deck, player, dealer)
+    dealer_play(deck, dealer)
+    ask_player_action()
+    run_full_game(deck, player, dealer)
 
 if __name__ == "__main__":
-    
-    run_full_game(deck, player, dealer)
+
+    init_game()
+
+
